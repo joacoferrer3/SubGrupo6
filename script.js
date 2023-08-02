3dcb61719be2e509ba3f599ddbf9792e19f1380a
joacoferrer3 <joacoferrer3@gmail.com>
2023-08-02 19:45:06 -0300

    scripot js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const imagenes = document.getElementsByTagName('img');
 
+    const nuevoAncho = 800;
+    const nuevoAlto = 600;
+
+    for (let i = 0; i < imagenes.length; i++) {
+        imagenes[i].style.width = nuevoAncho + 'px';
+        imagenes[i].style.height = nuevoAlto + 'px';
+    }
+});
