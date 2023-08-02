c110105fe8fac7ccd417b50ad46a2a47a3c773cd
joacoferrer3 <joacoferrer3@gmail.com>
2023-08-02 18:36:26 -0300

    tamano img
    Cambie el tamano de las imagenes para que todas sean de 600 x 800. Con un js en el DOM

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const imagenes = document.getElementsByTagName('img');
 
+    const ancho = 800;
+    const alto = 600;
+
+    for (let i=0; i<imagenes.length; i++){
+        imagenes[i].style.width = ancho + 'px';
+        imagenes[i].style.height = alto + 'px';
+    }
+});
